5b734cc7fb99fa554c058ac3beb473cd698bc05c
Kaua Ghizoni <kghizoni@gmail.com>
2024-03-27 21:54:14 -0300

    FEAT: Iniciado pagina de cadastro de produto

--- LanchesSenac/src/App.tsx
+++ LanchesSenac/src/App.tsx
@@ -3,6 +3,10 @@ import "./App.css";
 import PaginaInicial from "./Pages/PaginaInicial/PaginaInicial";
 import Autor from "./Pages/Autor/Autor";
 import Cardapio from "./Pages/Cardapio/Cardapio";
+import FormProduto from "./components/FormProduto/FormProduto";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 
 const App = () => {
   return (
@@ -12,6 +16,7 @@ const App = () => {
         <Route path="/admin" />
         <Route path="/autor" element={<Autor />} />
         <Route path="/cardapio" element={<Cardapio />} />
+        <Route path="/administrativo" element={<FormProduto/>} />
       </Routes>
     </Router>
   );
