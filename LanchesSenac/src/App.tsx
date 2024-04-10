495d0148d9e9f10c1531266b0ef288beb0141aa3
Rafael Floriano <rafael.floriano@gmail.com>
2024-04-10 19:24:52 -0300

    REFACT: adding page 'paginaFormProduto' and created a route to get new page

--- LanchesSenac/src/App.tsx
+++ LanchesSenac/src/App.tsx
@@ -3,9 +3,10 @@ import "./App.css";
 import PaginaInicial from "./Pages/PaginaInicial/PaginaInicial";
 import Autor from "./Pages/Autor/Autor";
 import Cardapio from "./Pages/Cardapio/Cardapio";
-import FormProduto from "./components/FormProduto/FormProduto";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+// import FormProduto from "./components/FormProduto/FormProduto";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min";
+import PaginaFormProduto from "./Pages/PaginaFormProduto/PaginaFormProduto";
 
 
 const App = () => {
@@ -16,7 +17,7 @@ const App = () => {
         <Route path="/admin" />
         <Route path="/autor" element={<Autor />} />
         <Route path="/cardapio" element={<Cardapio />} />
-        <Route path="/administrativo" element={<FormProduto/>} />
+        <Route path="/paginaFormProduto" element={<PaginaFormProduto/>} />
       </Routes>
     </Router>
   );
---
+++ LanchesSenac/src/Pages/PaginaFormProduto/PaginaFormProduto.tsx
@@ -0,0 +1,9 @@
+const PaginaFormProduto = () => {
+    return( 
+            <div>
+                <h1>Pagina de formulário de produtos</h1>
+            </div>
+    );
+}
+
+export default PaginaFormProduto;
