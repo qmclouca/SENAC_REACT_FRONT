0e5aaaf7307032f01f71bddaaab695b6fe7a9b8e
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2024-04-10 21:39:57 -0300

    exemplo de implementação completa de um componente

---
+++ LanchesSenac/src/Pages/Shares/Share.tsx
@@ -0,0 +1,16 @@
+import ShareComponent from "../../components/ShareComponent/ShareComponent";
+
+const Shares = () => {
+    return (
+      <>
+      <div>
+           <ShareComponent
+           symbol="PETR4"
+           />
+        </div>
+       </>
+
+    );
+};
+
+  export default Shares;
