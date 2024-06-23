ff3b9aecd1ae8f71e7020599c843239a6665811a
rafael <rafael.florianoc9@gmail.com>
2024-06-22 23:56:38 -0300

    FEAT: implemented a loading in pagination, and added pagination in response

--- LanchesSenac/src/Pages/Funcionario/Funcionario.tsx
+++ LanchesSenac/src/Pages/Funcionario/Funcionario.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import Table from 'react-bootstrap/Table';
 import FuncionarioClient from '../../client/FuncionarioClient';
 import './Funcionario.css';
-import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Placeholder, Row } from "react-bootstrap";
 import FuncionarioDto from '../../dto/FuncionarioDto';
+import Pagination from '../../interfaces/Pagination';
 
 const Funcionario = () => {
     const [search, setSearch] = useState<string>('');
@@ -11,8 +13,8 @@ const Funcionario = () => {
     useEffect(() => {
         const fetchFuncionarios = async () => {
             try {
-                const response = await FuncionarioClient.listAllFuncionarios();
-                setFuncionarios(response);
+                const response:Pagination = await FuncionarioClient.findPaginationOfFuncionariosBySearch("");
+                setFuncionarios(response.content);
             } catch (error) {
                 console.error('Erro ao buscar funcionários:', error);
             }
@@ -22,8 +24,8 @@ const Funcionario = () => {
 
     const searchByFuncionario = async (search: string) => {
         try {
-            const response = await FuncionarioClient.findFuncionariosBySearch(search);
-            setFuncionarios(response);
+            const response = await FuncionarioClient.findPaginationOfFuncionariosBySearch(search);
+            setFuncionarios(response.content);
         } catch (error) {
             console.error('Erro ao buscar funcionarios por pesquisa:', error);
         }
@@ -35,54 +37,51 @@ const Funcionario = () => {
     };
 
     return (
-        <div className="stack-of-records">
-            <Form onSubmit={handleSubmit}>
-                <Row>
-                    <Col xs="auto">
-                        <Form.Control
-                            type="text"
-                            placeholder="Search"
-                            className="mr-sm-2"
-                            value={search}
-                            onChange={(e) => setSearch(e.target.value)}
-                        />
-                    </Col>
-                    <Col xs="auto">
-                        <Button type="submit">Submit</Button>
-                    </Col>
-                </Row>
-            </Form>
-            <Container className='header-of-records'>
-                <Container>
-                    <Row className='header-row-records'>
-                        <Col sm={4}>
-                            <label>Id</label>
+        <Container fluid="md" style={{ marginTop: '20vh', padding: '4vh 2vw', borderRadius: '5px', backgroundColor: 'gray'}}>
+            <Row>
+                <Form onSubmit={handleSubmit}>
+                    <Row>
+                        <Col xs="auto">
+                            <Form.Control
+                                type="text"
+                                placeholder="Pesquisa por nome de funcionário"
+                                className="mr-sm-2"
+                                value={search}
+                                onChange={(e) => setSearch(e.target.value)}
+                            />
                         </Col>
-                        <Col>
-                            <label>Nome</label>
-                        </Col>
-                        <Col>
-                            <label>Cargo</label>
+                        <Col xs="auto">
+                            <Button type="submit">Pesquisar</Button>
                         </Col>
                     </Row>
-                </Container>
-                {funcionarios.map(funcionario => (
-                    <Container key={funcionario.id} className='conteiner-record'>
-                        <Row>
-                            <Col sm={4}>
-                                <label>{funcionario.id}</label>
-                            </Col>
-                            <Col>
-                                <label>{funcionario.nome}</label>
-                            </Col>
-                            <Col>
-                                <label>{funcionario.cargo}</label>
-                            </Col>
-                        </Row>
-                    </Container>
-                ))}
-            </Container>
-        </div>
+                </Form>
+            </Row>
+            <Row style={{marginTop: '10vh', cursor: 'pointer'}}>
+                {funcionarios != null && funcionarios.length > 0 ? (
+                <Table hover>
+                    <thead>
+                        <th>Id</th>
+                        <th>Nome</th>
+                        <th>Cargo</th>
+                    </thead>
+                    <tbody>
+                        {funcionarios.map(funcionario => (
+                        <tr>
+                            <td>{funcionario.id}</td>
+                            <td>{funcionario.nome}</td>
+                            <td>{funcionario.cargo}</td>
+                        </tr>
+                        ))}
+                    </tbody>
+                </Table>
+                ) : (
+                    <div className='flex justify-content-center align-items-center'>
+                        <img style={{ width: '22vw', height: '28vh'}} src="https://i.pinimg.com/originals/d9/f2/15/d9f21515b1e38d83e94fdbce88f623b6.gif" alt="loading"/>
+                    </div>
+                )}
+            </Row>
+
+        </Container>
     );
 };
 
